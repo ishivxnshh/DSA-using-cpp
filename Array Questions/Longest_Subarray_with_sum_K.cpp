b35440efa10d719bfdafbf7fef37a3208638092c
Shivansh Mittal <www.shivansh065@gmail.com>
2024-08-02 21:14:25 +0530

    more codes added

--- Array Questions/Longest_Subarray_with_sum_K.cpp
+++ Array Questions/Longest_Subarray_with_sum_K.cpp
@@ -2,14 +2,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 // } Driver Code Ends
-class Solution{
-    public:
-    int lenOfLongSubarr(int A[],  int n, int k) 
-    { 
-        //for only positive no.s
-        // // Brute Force Approach (O[n^3] - TLE) 
+class Solution
+{
+public:
+    int lenOfLongSubarr(int A[], int n, int k)
+    {
+        // // Brute Force Approach (O[n^3] - TLE)
         // int maxlen = 0;
         // for(int i=0;i<n;i++)
         // {
@@ -40,58 +39,73 @@ class Solution{
         // }
         // return maxlen;
 
-        // // Better Approach 2 (Using Hashing)
-        map<long long, int> preSumMap;
-        long long sum = 0;
-        int maxLen = 0;
-        for (int i = 0; i < n; i++)
+        // // Better Approach 2 +ves,-ves and 0s (Using Hashing)
+        // TC(ordered Map) => O[N * logN] TC(unordered Map) => O[N * 1]=Best Case and O[N*2]=Worst Case  SC => O[N]
+        // map<int, int> preSumMap;
+        // int sum = 0;
+        // int maxLen = 0;
+        // for (int i = 0; i < n; i++)
+        // {
+        //     sum += A[i];
+        //     if (sum == k)
+        //     {
+        //         maxLen = i + 1;
+        //     }
+        //     int rem = sum - k;
+        //     if (preSumMap.find(rem) != preSumMap.end())
+        //     {
+        //         int len = i - preSumMap[rem];
+        //         maxLen = max(maxLen, len);
+        //     }
+        //     if (preSumMap.find(sum) == preSumMap.end())
+        //     {
+        //         preSumMap[sum] = i;
+        //     }
+        // }
+        // return maxLen;
+
+        // // Optimal Approach for +ves and 0s (Using 2 Pointers) TC => O[2N] SC => O[1]
+        int left = 0, right = 0;
+        int sum = 0;
+        int maxlen = 0;
+        while (right < n)
         {
-            sum += A[i];
-            if (sum == k) 
+            sum += A[right];
+            while (left <= right && sum > k)
             {
-                maxLen = max(maxLen, i + 1);
+                sum -= A[left];
+                left++;
             }
-            int rem = sum - k;
-            if (preSumMap.find(rem) != preSumMap.end()) 
+            if (sum == k)
             {
-                int len = i - preSumMap[rem];
-                maxLen = max(maxLen, len);
+                maxlen = max(maxlen, right - left + 1);
             }
-            if (preSumMap.find(sum) == preSumMap.end()) 
-            {
-                preSumMap[sum] = i;
-            }
+            right++;
         }
-        return maxLen;
-
-        // // Optimal Approach (Using 2 Pointers)
-
-
-
-        
-    } 
-
+        return maxlen;
+    }
 };
 
 //{ Driver Code Starts.
 
-int main() {
-	//code
-	
-	int t;cin>>t;
-	while(t--)
-	{
-	    int n, k;
-	    cin>> n >> k;
-	    int a[n];
-	    
-	    for(int i=0;i<n;i++)
-	        cin>>a[i];
-	   Solution ob;
-	   cout << ob.lenOfLongSubarr(a, n , k)<< endl;
-	    
-	}
-	
-	return 0;
+int main()
+{
+    // code
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        int a[n];
+
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        Solution ob;
+        cout << ob.lenOfLongSubarr(a, n, k) << endl;
+    }
+
+    return 0;
 }
 // } Driver Code Ends
