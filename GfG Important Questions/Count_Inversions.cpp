d3f5b6a2dc2b0f05dc7e84c778636f13ab2d4e77
Shivansh Mittal <www.shivansh065@gmail.com>
2025-01-23 23:57:13 +0530

    added

---
+++ GfG Important Questions/Count_Inversions.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to count inversions in the array.
+    int inversionCount(vector<int> &arr) {
+
+        //Brute-Force Approach(tc:O(n^2), sc:O(1)) (tcs passed: 1110/1115)
+        // int cnt = 0;
+        // for(int i=0;i<arr.size();i++)
+        // {
+        //     for(int j=i+1;j<arr.size();j++)
+        //     {
+        //         if(arr[i]>arr[j])
+        //             cnt++;
+        //     }
+        // }
+        // return cnt;
+
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int T;
+    cin >> T;
+    cin.ignore();
+    while (T--) {
+        int n;
+        vector<int> a;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+        Solution obj;
+        cout << obj.inversionCount(a) << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
--- GfG Important Questions/Missing_And_Repeating.cpp
+++ GfG Important Questions/Missing_And_Repeating.cpp
@@ -3,12 +3,13 @@
 
 using namespace std;
 
-
 // } Driver Code Ends
-class Solution {
-  public:
-    vector<int> findTwoElement(vector<int>& arr) {
-        
+class Solution
+{
+public:
+    vector<int> findTwoElement(vector<int> &arr)
+    {
+
         // Brute-Force Approach (tc:n^2, sc:1) (tcs passed: 1110/1111)
         // int n = arr.size();
         // int a = -1;
@@ -30,70 +31,101 @@ class Solution {
         // }
         // return {a, b};
 
-        //Better Approach (tc: O(n)+O(n)=O(n), sc: O(n)) (all tcs passed)
-        // int n = arr.size();
-        // int hash[n+1] = {0};
-        // int rep = -1, miss = -1;
-        // for(int i=0;i<n;i++)
-        // {
-        //     hash[arr[i]]++;
-        // }
-        // for(int i=1;i<=n;i++)
-        // {
-        //     if(hash[i]==2)
-        //         rep = i;
-        //     else if(hash[i]==0)
-        //         miss = i;
-        //     if(rep!=-1 && miss!=-1)
-        //         break;
-        // }
-        // return {rep, miss};
+        // Better Approach (tc: O(n)+O(n)=O(n), sc: O(n)) (all tcs passed)
+        //  int n = arr.size();
+        //  int hash[n+1] = {0};
+        //  int rep = -1, miss = -1;
+        //  for(int i=0;i<n;i++)
+        //  {
+        //      hash[arr[i]]++;
+        //  }
+        //  for(int i=1;i<=n;i++)
+        //  {
+        //      if(hash[i]==2)
+        //          rep = i;
+        //      else if(hash[i]==0)
+        //          miss = i;
+        //      if(rep!=-1 && miss!=-1)
+        //          break;
+        //  }
+        //  return {rep, miss};
 
-        //Optimal Approach 1 (using maths soln) (tc: n,sc: 1)
-        // int n = arr.size();
-        // long long total = (long long)n * (n + 1) / 2;
-        // long long sq_total = (long long)n * (n + 1) * (2 * n + 1) / 6;
-    
+        // Optimal Approach 1 (using maths soln) (tc: n,sc: 1)
+        //  int n = arr.size();
+        //  long long total = (long long)n * (n + 1) / 2;
+        //  long long sq_total = (long long)n * (n + 1) * (2 * n + 1) / 6;
+
         // long long sum = 0, sq_sum = 0;
         // for (int i = 0; i < n; i++) {
         //     sum += arr[i];
         //     sq_sum += (long long)arr[i] * arr[i];
         // }
-    
+
         // long long val1 = sum - total; // x - y
         // long long val2 = (sq_sum - sq_total) / val1; // x + y
-    
+
         // long long x = (val1 + val2) / 2; // Duplicate
         // long long y = x - val1; // Missing
-    
+
         // return {(int)x, (int)y};
 
-        //Optimal Approach 2 (using XOR soln) (tc: n,sc: 1)
+        // Optimal Approach 2 (using XOR soln) (tc: n,sc: 1)
         int n = arr.size();
         int xr = 0;
-        for (int i = 0; i < n; i++) 
+        for (int i = 0; i < n; i++)
         {
-            xr^=arr[i];
-            xr^=(i+1);
+            xr ^= arr[i];
+            xr ^= (i + 1);
         }
-        cout<<xr;
         int bitNo = 0;
+        while (1)
+        {
+            if ((xr & (1 << bitNo)) != 0)
+                break;
+            bitNo++;
+        }
+        int zero = 0;
+        int one = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] & (1 << bitNo))
+                one ^= arr[i];
+            else
+                zero ^= arr[i];
+            if ((i + 1) & (1 << bitNo))
+                one ^= (i + 1);
+            else
+                zero ^= (i + 1);
+        }
+        int cnt = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] == zero)
+                cnt++;
+        }
+        if (cnt == 2)
+            return {zero, one};
+        else
+            return {one, zero};
     }
 };
 
 //{ Driver Code Starts.
 
-int main() {
+int main()
+{
     int t;
     cin >> t;
     cin.ignore();
-    while (t--) {
+    while (t--)
+    {
         string input;
         int num;
         vector<int> arr;
         getline(cin, input);
         stringstream s2(input);
-        while (s2 >> num) {
+        while (s2 >> num)
+        {
             arr.push_back(num);
         }
         Solution ob;
