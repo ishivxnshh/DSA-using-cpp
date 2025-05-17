75f68448d3db64f801842f89a38c6183dbe974fe
Shivansh Mittal <www.shivansh065@gmail.com>
2025-05-17 18:15:07 +0530

    added

--- LinkedList/deletion.cpp
+++ LinkedList/deletion.cpp
@@ -55,10 +55,7 @@ Node* deleteHead(Node* head)
 
 Node* deleteTail(Node* head)
 {
-    if(head == NULL || head -> next == NULL)
-    {
-        return NULL;
-    }
+    if(head == NULL || head -> next == NULL) return NULL;
     Node* temp = head;
     while(temp -> next -> next != NULL)
     {
@@ -66,20 +63,76 @@ Node* deleteTail(Node* head)
     }
     delete temp -> next;
     temp -> next = nullptr;
+    return head; 
+}
+
+Node* deleteKthElement(Node* head, int k)
+{
+    if(head == NULL)
+    {
+        return head;
+    }
+    if(k == 1)
+    {
+        Node* temp = head;
+        head = head -> next;
+        delete temp;
+        return head;
+    }
+    int cnt = 0;
+    Node* temp = head;
+    Node* prev = NULL;
+    while(temp != NULL)
+    {
+        cnt++;
+        if(cnt == k)
+        {
+            prev -> next = prev -> next -> next;
+            delete temp;
+            break;
+        }
+        prev = temp;
+        temp = temp -> next;
+    }
     return head;
 }
 
 Node* deleteK(Node* head, int k)
 {
     if(head == NULL)
+    {
         return NULL;
+    }
+    if (head->data == k)
+    {
+        Node* temp = head;
+        head = head -> next;
+        delete temp;
+        return head;
+    }
+    Node* temp = head;
+    Node* prev = NULL;
+    while(temp != NULL)
+    {
+        if(temp -> data == k)
+        {
+            prev -> next = prev -> next -> next;
+            free(temp);
+            break;
+        }
+        prev = temp;
+        temp = temp -> next;
+    }
+    return head;
 }
 
 int main() 
 {
-    vector<int> arr = {12, 5, 8, 7};
+    vector<int> arr = {12, 5, 8, 7, 11};
     Node* head = covertArr2LL(arr);
     // head = deleteHead(head);
     // head = deleteTail(head);
+    // head = deleteKthElement(head, 3);
+    head = deleteK(head, 7);
     print(head);
 }
