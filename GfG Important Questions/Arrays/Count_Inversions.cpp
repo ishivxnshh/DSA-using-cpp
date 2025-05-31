83a93ac88cad2519a6187923a638d9c4c562f878
Shivansh Mittal <www.shivansh065@gmail.com>
2025-05-31 16:05:32 +0530

    added

--- GfG Important Questions/Arrays/Count_Inversions.cpp
+++ GfG Important Questions/Arrays/Count_Inversions.cpp
@@ -7,7 +7,53 @@ using namespace std;
 class Solution {
   public:
 
+    int merge(vector<int> &arr, int low, int mid, int high)
+    {
+        vector<int> temp;
+        int left = low;
+        int right = mid + 1;
+        int count = 0;
+        while(left <= mid && right <= high)
+        {
+            if(arr[left] <= arr[right])
+            {
+                temp.push_back(arr[left]);
+                left++;
+            }
+            else
+            {
+                temp.push_back(arr[right]);
+                right++;
+                count += (mid - left + 1);
+            }
+        }
+        while(left <= mid)
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        while(right <= high)
+        {
+            temp.push_back(arr[right]);
+            right++;
+        }
+        for(int i = low; i <= high; i++)
+        {
+            arr[i] = temp[i - low];
+        }
+        return count;
+    }
     
+    int mergeSort(vector<int> &arr, int low, int high)
+    {
+        int count = 0;
+        if(low >= high) return;
+        int mid = (low + high) / 2;
+        count += mergeSort(arr, low, mid);
+        count += mergeSort(arr, mid + 1, high);
+        count += merge(arr, low, mid, high);
+        return count;
+    }
 
     // Function to count inversions in the array.
     int inversionCount(vector<int> &arr) {
@@ -24,8 +70,8 @@ class Solution {
         // }
         // return cnt;
 
-        //  Optimized Approach(tc:O(nlogn), sc:O(n))
-        
+        //  Optimal Approach(tc:O(nlogn), sc:O(n))
+        return mergeSort(arr, 0, arr.size() - 1);
     }
 };
 
