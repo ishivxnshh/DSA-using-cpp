fc1ba873ffddcd287e81d370743aaa82e4836a76
Shivansh Mittal <www.shivansh065@gmail.com>
2025-06-07 18:03:39 +0530

    added

---
+++ GfG Important Questions/Binary Search/Median_in_a_row_wise_sorted_Matrix.cpp
@@ -0,0 +1,71 @@
+// User function template for C++
+
+class Solution
+{
+public:
+    // Custom implementation of upper_bound
+    int upperBound(vector<int> &row, int target)
+    {
+        int low = 0;
+        int high = row.size();
+
+        while (low < high)
+        {
+            int mid = (low + high) / 2;
+
+            if (row[mid] <= target)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid;
+            }
+        }
+
+        return low;
+    }
+
+    int countSmallEqual(vector<vector<int>> &mat, int n, int m, int target)
+    {
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            count += upperBound(mat[i], target);
+        }
+        return count;
+    }
+
+    int median(vector<vector<int>> &mat)
+    {
+        int n = mat.size();
+        int m = mat[0].size();
+        int low = INT_MAX;
+        int high = INT_MIN;
+
+        for (int i = 0; i < n; i++)
+        {
+            low = min(low, mat[i][0]);
+            high = max(high, mat[i][m - 1]);
+        }
+
+        int req = (n * m) / 2;
+
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            int smallEqual = countSmallEqual(mat, n, m, mid);
+
+            if (smallEqual <= req)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+
+        return low;
+    }
+};
